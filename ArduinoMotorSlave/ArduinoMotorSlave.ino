910a1bc9a20a7896071a1e2d3ba3d497ef12ac79
samynv <d.diks@mail.blastedsolutions.com>
2013-09-26 13:59:21 +0200

    Header

--- ArduinoMotorSlave/ArduinoMotorSlave.ino
+++ ArduinoMotorSlave/ArduinoMotorSlave.ino
@@ -1,15 +1,4 @@
-#include <Wire.h>
-#include <Stepper.h>
-#include "Queue.h"
-
-//Globale variabelen declareren
-//Stappenmotoren
-Stepper leftFront = Stepper(200, A0, A1);
-Stepper leftBehind = Stepper(200, A2, A3);
-Stepper rightFront = Stepper(200, 6, 7);
-Stepper rightBehind = Stepper(200, 8, 9);
-//Scheduler
-Queue myQueue;
+#include "ArduinoMotorSlave.h"
 
 void setup() 
 {
