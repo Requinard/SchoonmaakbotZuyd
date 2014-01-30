9a479b11ea0c09e3789b8cdbf40a7e890a4b352f
Samyn <daviddiks91@hotmail.com>
2014-01-30 10:39:54 +0100

    Werkende pathfinding

--- OpenCV/pathfinding.py
+++ OpenCV/pathfinding.py
@@ -1,7 +1,10 @@
 __author__ = 'David'
 
+import random
+import math
+
 from numpy import *
-from random import random
+from operator import itemgetter
 
 
 """
@@ -20,6 +23,43 @@ Indeling grid is als volgt:
 grid[Y][X]
 """
 
+"""
+Houdt de data voor een node vast.
+"""
+class Node():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.plus = 0
+        self.minus = 0
+
+    def __int__(self):
+        return self.plus
+
+    def __str__(self):
+        return str("%i, %y" % (self.x, self.y))
+
+    def __getitem__(self, item):
+        if item == "plus":
+            return self.plus
+        elif item == "minus":
+            return self.minus
+
+    #Sorteert op x+y
+    def SortPlus(self, originX, originY):
+        self.plus = math.fabs((self.x - originX) + (self.y - originY))
+        return self.plus
+
+    #Sorteren op X-Y
+    def SortMinus(self, originX, originY):
+        self.minus = math.fabs((self.x - originX) - (self.y - originY))
+        return self.minus
+
+"""
+Genereert een grid en schrijft dit naar grid.txt.
+
+Verzorgt ook een automatische lijst met positieve nodes.
+"""
 def generateGrid():
     #GRid is gemaakt van 10 centimeter vlakken. 400x400
     grid = zeros((40,40), dtype=bool)
@@ -27,16 +67,31 @@ def generateGrid():
     #bierkrat vullen
     for y in range(0, 3):
         for x in range(0, 5):
-            grid[35+x][y] = True
+            grid[35+x][37+y] = True
 
     #Robot zetten
-    grid[0][39] = True
+    grid[0][0] = True
 
-    f = open("grid.txt", 'w')
+    #Lijst aanmaken voor nodes
+    nodes  = list()
+
+    #Flesjes dumpen. ZUIPEN
+    for cnt in range(0, 12):
+        while True:
+            x = random.randint(0, 40)
+            y = random.randint(0, 40)
+
+            if not grid[y][x]:
+                nodes.append(Node(x, y))
+                grid[y][x] = True
+                break
+
+
+        f = open("grid.txt", 'w')
 
     for x in range(0, 40):
         for y in range(0,40):
-            if grid[x][y] == True:
+            if grid[y][x] == True:
                 f.write('1 ')
             else:
                 f.write('0 ')
@@ -46,8 +101,69 @@ def generateGrid():
 
     set_printoptions(threshold='nan')
 
-    print grid
+    return (grid, nodes)
+
+
+"""
+Begint met een lijst nodes.
+
+Past vervolgens snelste pad toe
+"""
+def pathfinding(nodes):
+    #Startpunt
+    currentLocation = Node(0,0)
+    #Route
+    Route = list()
+
+    #Punten berekenen
+    while True:
+        #Nodes hun SortPlus geven
+        for node in nodes:
+            node.SortPlus(currentLocation.x, currentLocation.y)
+            print "Node nummer %i op punt %f" % (1, node.plus)
+
+        #Nodes sorteren op hun SortPlus
+        nodes.sort(key=itemgetter("plus"))
+
+        #Lijst met eindresultaten aanmaken
+        possibleNodes = list()
+
+        for i in range(0,5):
+            #Als dit de kleinste overgebleven node is
+            if nodes[i].plus < nodes[i+1].plus:
+                possibleNodes.append(nodes[i])
+                break
+            #Als er nog even grote nodes over zijn.
+            elif nodes[i].plus == nodes[i+1].plus:
+                possibleNodes.append(nodes[i])
+
+        #Als er meer als een node is gevonden moet er gesorteerd worden op minus
+        if len(possibleNodes) != 1:
+            #Minus berekenen
+            for node in possibleNodes:
+                node.SortMinus(currentLocation.x, currentLocation.y)
+
+            #Sorteren
+            possibleNodes.sort(key=itemgetter("minus"))
+
+
+        #locatie updaten en gebruikte node verwijderen
+        currentLocation = Node(possibleNodes[0].x, possibleNodes[0].y)
+
+        for i in range(0, len(nodes)):
+            if nodes[i] == possibleNodes[0]:
+                nodes.pop(i)
+                break
+
+        Route.append(possibleNodes[0])
+
+        if len(Route) == 6:
+            break
+    return Route
+
+
 
 def run():
-    generateGrid()
+    grid, nodes = generateGrid()
+    Route = pathfinding(nodes)
 
